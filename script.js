af2cc2c3b6fd4eed9020e88b3e4a024866108162
NazariySokol94 <113379693+NazariySokol94@users.noreply.github.com>
2022-12-25 22:18:50 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const dino = document.getElementById("dino");
+const cactus = document.getElementById("cactus");
+
+document.addEventListener("keydown", function(event) {
+    jamp();
+});
+function jamp() {
+    if (dino.classList != "jamp") {
+        dino.classList.add("jamp")
+    }
+    setTimeout( function() {
+        dino.classList.remove("jamp")
+    }, 300)
+}
+let isAlive = setInterval ( function() {
+    let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"))
+    let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"))
+    if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
+        alert('GAME OVER')
+    }
+}, 10)
